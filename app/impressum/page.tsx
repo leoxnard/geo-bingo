59a04236c21d92fd37349cb6a29949d10faff9fc
Leonard <leonardsima02@gmail.com>
2026-04-19 04:00:01 +0200

    impressum

--- app/impressum/page.tsx
+++ app/impressum/page.tsx
@@ -1,22 +1,24 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Impressum() {
     return (
         <main className="flex min-h-screen flex-col items-center p-24 bg-slate-900 text-white leading-relaxed">
-            <div className="bg-slate-800 p-8 rounded-2xl shadow-2xl border border-slate-700 w-full max-w-md flex flex-col gap-6 mt-12">
+            <div className="bg-slate-800 p-8 rounded-2xl shadow-2xl border border-slate-700 w-full max-w-md flex flex-col gap-3 mt-12">
                 <h1 className="text-3xl font-bold text-indigo-400 tracking-tighter mb-2">Legal Notice</h1>
-                
+
                 <div>
                     <h2 className="font-semibold text-lg text-slate-200">Information pursuant to § 5 TMG</h2>
-                    <p className="mt-2 text-slate-400">Leonard Sima</p>
-                    <p className="text-slate-400">Floriansmühlstrasse 1</p>
-                    <p className="text-slate-400">80939 Munich</p>
-                    <p className="text-slate-400">Germany</p>
+                    <div className="">
+                        <Image src="/images/address-info.png" alt="Address Details" width={270} height={60} className="pointer-events-none select-none" />
+                    </div>
                 </div>
 
                 <div>
                     <h2 className="font-semibold text-lg text-slate-200 mt-2">Contact</h2>
-                    <p className="text-slate-400">Email: geobingo@leonardsima.de</p>
+                    <div className="">
+                        <Image src="/images/email-info.png" alt="Contact Email" width={270} height={60} className="pointer-events-none select-none" />
+                    </div>
                 </div>
 
                 <div className="w-full h-px bg-slate-700 my-2"></div>
